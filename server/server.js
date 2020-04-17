79ac193bce22af69971a40388316a81fa40ec79f
somamizobuchi <soma.mizo@gmail.com>
2020-04-17 00:50:29 -0400

    server updates

--- server/server.js
+++ server/server.js
@@ -9,7 +9,8 @@ const cors = require('cors');
 // Database connection
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 }, (err) => {
     if(!err){
         console.log("Connected to database")
