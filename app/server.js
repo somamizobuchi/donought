93f00647edfaaa4534d77382e7fba66a54289734
somamizobuchi <soma.mizo@gmail.com>
2020-06-14 13:38:22 -0400

    heroku

--- app/server.js
+++ app/server.js
@@ -42,9 +42,9 @@ app.use('/api', apiRouter); // Rest API methods
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
     const path = require('path');
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
+    app.get("/*", (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     })
 }
