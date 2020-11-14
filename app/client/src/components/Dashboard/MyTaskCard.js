11c97762a8b172d3105f8038a6b3ebcac8e93c8d
somamizobuchi <soma.mizo@gmail.com>
2020-11-13 22:49:30 -0500

    removing whitespace from streak id

--- app/client/src/components/Dashboard/MyTaskCard.js
+++ app/client/src/components/Dashboard/MyTaskCard.js
@@ -68,8 +68,8 @@ export default function TaskCard(props) {
 			padding: 0
 		}
 		return (
-			<Col className="text-center pl-0" >
-				<div id={target} style={style} className={"bg-" + props.color}></div>
+			<div className="col row">
+				<div id={target} className={"col" + "bg-" + props.color} style={style}></div>
 				<Tooltip
 					isOpen={tooltipOpen}
 					toggle={toggleTooltip}
@@ -78,7 +78,7 @@ export default function TaskCard(props) {
 				>
 					{date}
 				</Tooltip>
-			</Col>
+			</div>
 		)
 	}
 
@@ -97,13 +97,13 @@ export default function TaskCard(props) {
 				if (logDate.isBetween(day.startOf('day'), day.endOf('day'))) {
 					let color = log.success ? "success" : "danger"
 					date = date + " " + log.comment;
-					items.push(<Streak key={i} color={color} today={i === 0} date={date} target={task.title + i} />)
+					items.push(<Streak key={i} color={color} today={i === 0} date={date} target={task.title.replace(/ /g, '') + i} />)
 					return true
 				} else {
 					return false
 				}
 			})
-			if (!flag) items.push(<Streak key={i} color="light" today={i === 0} date={date} target={task.title + i} />)
+			if (!flag) items.push(<Streak key={i} color="light" today={i === 0} date={date} target={task.title.replace(/ /g, '') + i} />)
 		}
 		setStreaks(items)
 	}, [])
@@ -144,9 +144,9 @@ export default function TaskCard(props) {
 				<LogFormModal refresh={{ refresh, setRefresh }} tid={task._id} alert={{ alert, setAlert }} modal={modal} setModal={setModal} />
 				{/* Streak */}
 				<Container>
-					<Row>
+					<div className="row">
 						{streaks}
-					</Row>
+					</div>
 				</Container>
 			</CardBody>
 			<CardFooter>
